70b2705a49d04ac6773722b596edde2cc9013089
alinanefedowa <nefedowa.as@gmail.com>
2022-04-10 18:15:09 +0300

    improved tests

--- src/test/java/guru/qa/StudentRegistrationFormTestsImproved.java
+++ src/test/java/guru/qa/StudentRegistrationFormTestsImproved.java
@@ -6,29 +6,32 @@ package guru.qa;
         import org.junit.jupiter.api.BeforeEach;
         import org.junit.jupiter.api.Test;
         import org.openqa.selenium.By;
-
+        import javax.swing.*;
         import static com.codeborne.selenide.Condition.text;
         import static com.codeborne.selenide.Condition.visible;
         import static com.codeborne.selenide.Selectors.byText;
         import static com.codeborne.selenide.Selenide.*;
+        import static java.lang.String.format;
         import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentRegistrationFormTestsImproved {
 
-
-    String firstName = "Alisa";
-    String lastName = "Berg";
-    String email = "alisa.berg@gmail.com";
-    String gender = "Female";
-    String mobile = "9213332221";
-    String dateOfBirth = "12 April,1992";
-    String subjects = "Computer science";
-    String hobby1 = "Sports";
-    String hobby2 = "Reading";
-    String picture = "Picture.jpeg";
-    String currentAddress = "Operngasse 15, 7";
-    String state = "Haryana";
-    String city = "Karnal";
+    String firstName = "Alisa",
+           lastName = "Berg",
+           email = "alisa.berg@gmail.com",
+           gender = "Female",
+           mobile = "9213332221",
+           dateOfBirth = "30 April,1992",
+           subjects = "Computer science",
+           hobby1 = "Sports",
+           hobby2 = "Reading",
+           picture = "Picture.jpeg",
+           currentAddress = "Operngasse 15, 7",
+           state = "Haryana",
+           city = "Karnal";
+    String expectedFullName = format("%s %s", firstName, lastName),
+           expectedStateAndCity = format("%s %s", state, city),
+           expectedHobbies = format("%s, %s", hobby1, hobby2);
 
     @BeforeAll
     static void setUp() {
@@ -40,23 +43,25 @@ public class StudentRegistrationFormTestsImproved {
     void fillFormTest() {
 
         open("/automation-practice-form");
-
-        //Hide the footer and banner
-        Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';" +
-                "document.querySelector(\"#fixedban\").hidden = 'true'");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $(byText(gender)).click();
+        $("#genterWrapper").$(byText(gender)).click();
+        //$("#gender-radio-2").parent().click();
         $("#userNumber").setValue(mobile);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1992");
-        $("[aria-label$='April 12th, 1992']").click();
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+
         $("#subjectsInput").setValue(subjects).pressEnter();
-        $(byText(hobby1)).click();
-        $(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        //$("#hobbies-checkbox-1").parent().click();
         $("#uploadPicture").uploadFromClasspath("img/"+picture);
         $("#currentAddress").setValue(currentAddress);
 
@@ -68,26 +73,22 @@ public class StudentRegistrationFormTestsImproved {
         $("#city").click();
         $("#city").$(byText(city)).click();
 
-        //Click "Submit" button
         $("#submit").click();
-
-        //Click "Submit" with changed page zoom
-        // executeJavaScript("document.body.style.zoom='0.75';document.getElementById(\"submit\").click()");
 
         //Assertions
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(
-                text(firstName + " " + lastName),
+        $(".table-responsive").shouldHave(
+                text(expectedFullName),
                 text(email),
                 text(gender),
                 text(mobile),
                 text(dateOfBirth),
                 text(subjects),
-                text(hobby1 + ", " + hobby2),
+                text(expectedHobbies),
                 text(picture),
                 text(currentAddress),
-                text(state + " " + city)
+                text(expectedStateAndCity)
         );
     }
 }
